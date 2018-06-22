d7e3f6aa4eedb57dd232fe4481a207e55fdf75e1
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-22 10:00:16 -0400

    Some cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,4 @@ Rails.application.routes.draw do
     :sessions => "milia/sessions", 
     :passwords => "milia/passwords", 
   }
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
